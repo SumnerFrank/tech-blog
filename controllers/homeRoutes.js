bbc40ad3c9d95e47ca4b4aba2bb48b2baa63b8c5
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-19 14:34:56 -0500

    added home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,16 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models')
+
+router.get('/', (req, res) => {
+    Post.findAll({
+
+    })
+    .then(dbPostData => {
+        //render homepage with posts
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
