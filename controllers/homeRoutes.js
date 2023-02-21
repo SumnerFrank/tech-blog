5b5c4766a8310a5ab65d622bd45f70bca04630ef
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-21 07:21:51 -0500

    findAll home route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,10 +3,34 @@ const { Post, User, Comment } = require('../models')
 
 router.get('/', (req, res) => {
     Post.findAll({
-
+        attributes: [
+            'id',
+            'post_title',
+            'post_body',
+            'user_id'
+        ], 
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_body',
+                    'post_id',
+                    'user_id',
+                ],
+                include: {
+                    model: User,
+                    attrubutes: ['name']
+                }
+            }
+        ]
     })
     .then(dbPostData => {
-        //render homepage with posts
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', {
+            posts, 
+            loggedIn: req.session.loggedIn
+        });
     })
     .catch(err => {
         console.log(err);
