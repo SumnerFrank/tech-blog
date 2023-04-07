b4d666c66acc01dbace2d7adf3331b526c929cad
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-04-06 20:15:47 -0400

    added logout page and logout logic, added dashboard.handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,3 @@
-console.log('Hello World!');
-
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
@@ -17,7 +15,7 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
