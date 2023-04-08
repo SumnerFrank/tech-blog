8db0eae967e7a08e746634b938f82b92eeb8d115
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-04-08 08:13:50 -0400

    updated readme

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-// Dependencies
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const session = require('express-session')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// routers
-const sequelize = require('./config/connection');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Server
 const app = express();
